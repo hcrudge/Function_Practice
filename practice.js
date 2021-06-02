e86fefd0e24e3e36ecf06dc6ac4c2b5a0c880a87
hcrudge <hcrudge1@gmail.com>
2021-06-02 10:43:03 -0500

    functionpractice1

---
+++ practice.js
@@ -0,0 +1,37 @@
+
+// Introduction: Age in Dog Years Section - 
+
+const calculateAgeInDogYears = (ageOfPerson) =>{
+    const ageInDogYears = ageOfPerson / 7
+
+    return ageInDogYears
+}
+
+const dogAge = calculateAgeInDogYears(56)
+// console.log(dogAge)
+
+
+// Practice: Best in Show Section - 
+
+const favoriteDogBreed = (dogBreed) => {
+    if (dogBreed === "meow"){
+    return "I like cats!"    
+    }
+    return "my favorite dog breed is " + dogBreed
+}
+const doggy = favoriteDogBreed("meow")
+// console.log(`When it comes to pets,${doggy}`)
+
+// Practice: Addition Section - 
+
+const add = (first, second, third) => {
+    const sum = first + second + third
+    return sum 
+}
+const addValue = add(17,4,11)
+// console.log(addValue)
+
+// Practice: Self-Driving Cars
+
+
+
